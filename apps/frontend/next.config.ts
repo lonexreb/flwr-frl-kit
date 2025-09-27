7d5a89338c3f51a4b9df4ae41f2623e2aaf52bc9
sethimage <philcuvin@gmail.com>
2025-09-27 16:41:09 -0700

    compatible with railway without build

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  outputFileTracingRoot: '/app/apps/frontend',
+  eslint: {
+    ignoreDuringBuilds: true
+  }
 };
 
 export default nextConfig;
